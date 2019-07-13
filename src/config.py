5dd30dac667ca856f4e5ce539501bc27ebdc36a7
PedroDiSanti <pedroa.disanti@gmail.com>
2019-07-13 13:31:41 -0300

    starting flask

---
+++ src/config.py
@@ -0,0 +1,13 @@
+import os
+
+
+class Development(object):
+    DEBUG = True
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+app_config = {
+    'development': Development,
+    'default': Development
+}
